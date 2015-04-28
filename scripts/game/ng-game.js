2fe6e36f54d17155bc1ca755d06940db2b86a15a
DangerRanger <dangerousdevops@gmail.com>
2015-04-28 14:22:48 -0700

    view outline, game state, start game controller

--- scripts/game/ng-game.js
+++ scripts/game/ng-game.js
@@ -1,7 +1,8 @@
 angular.module('app', [
   // dependencies
   'ui.router',
-  'app.menu'
+  'app.menu',
+  'app.game'
 ]);
 // the reason for using ui.router is so that each module will be responsible
 // for building their own route dependencies
