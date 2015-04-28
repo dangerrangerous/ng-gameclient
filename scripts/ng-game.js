9343f63b88ceb4fdeac7daea3a0c88a0e30d4b1d
DangerRanger <dangerousdevops@gmail.com>
2015-04-28 13:10:31 -0700

    the kids want angular

--- scripts/ng-game.js
+++ scripts/ng-game.js
@@ -1,4 +1,7 @@
 angular.module('app', [
   // dependencies
-  'ui.router'
+  'ui.router',
+  'app.menu'
 ]);
+// the reason for using ui.router is so that each module will be responsible
+// for building their own route dependencies
